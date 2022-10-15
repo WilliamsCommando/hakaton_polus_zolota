de8f2cc7ff2c533cdf20951e232b0a781dfdf68f
Williamc Commando <williamc@student.21-school.ru>
2022-10-15 06:18:14 +0300

    Календарик ура

--- calendar.js
+++ calendar.js
@@ -1,67 +1,136 @@
-let nowDate = new Date(),
-    nowDateNumber = nowDate.getDate(),
-    nowMonth = nowDate.getMonth(),
-    nowYear = nowDate.getFullYear(),
-    container = document.getElementById('month-calendar'),
-    monthContainer = container.getElementsByClassName('month-name')[0],
-    yearContainer = container.getElementsByClassName('year-name')[0],
-    daysContainer = container.getElementsByClassName('days')[0],
-    prev = container.getElementsByClassName('prev')[0],
-    next = container.getElementsByClassName('next')[0],
-    monthName = ['январь','февраль','март','апрель','май','июнь','июль','август','сентябрь','октябрь','ноябрь','декабрь'];
+function generate_year_range(start, end) {
+    var years = "";
+    for (var year = start; year <= end; year++) {
+        years += "<option value='" + year + "'>" + year + "</option>";
+    }
+    return years;
+}
+
+today = new Date();
+currentMonth = today.getMonth();
+currentYear = today.getFullYear();
+selectYear = document.getElementById("year");
+selectMonth = document.getElementById("month");
+
+
+createYear = generate_year_range(1970, 2050);
+/** or
+ * createYear = generate_year_range( 1970, currentYear );
+ */
+
+document.getElementById("year").innerHTML = createYear;
+
+var calendar = document.getElementById("calendar");
+var lang = calendar.getAttribute('data-lang');
+
+var months = "";
+var days = "";
+
+var monthDefault = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var dayDefault = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+if (lang == "en") {
+    months = monthDefault;
+    days = dayDefault;
+} else if (lang == "id") {
+    months = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+    days = ["Ming", "Sen", "Sel", "Rab", "Kam", "Jum", "Sab"];
+} else if (lang == "fr") {
+    months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+    days = ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"];
+} else {
+    months = monthDefault;
+    days = dayDefault;
+}
+
+
+var $dataHead = "<tr>";
+for (dhead in days) {
+    $dataHead += "<th data-days='" + days[dhead] + "'>" + days[dhead] + "</th>";
+}
+$dataHead += "</tr>";
+
+//alert($dataHead);
+document.getElementById("thead-month").innerHTML = $dataHead;
+
+
+monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
 
 
 
-let curDate = nowDate.setMonth(nowDate.getMonth() - 1);
-console.log(nowDate.getFullYear());
+function next() {
+    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+    currentMonth = (currentMonth + 1) % 12;
+    showCalendar(currentMonth, currentYear);
+}
 
-function setMonthCalendar(year,month) {
-    let monthDays = new Date(year, month + 1, 0).getDate(),
-        monthPrefix = new Date(year, month, 0).getDay(),
-        monthDaysText = '';
+function previous() {
+    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+    showCalendar(currentMonth, currentYear);
+}
 
-    monthContainer.textContent = monthName[month];
-    yearContainer.textContent = year;
-    daysContainer.innerHTML = '';
+function jump() {
+    currentYear = parseInt(selectYear.value);
+    currentMonth = parseInt(selectMonth.value);
+    showCalendar(currentMonth, currentYear);
+}
 
-    if (monthPrefix > 0){
-        for (let i = 1  ; i <= monthPrefix; i++){
-            monthDaysText += '<li></li>';
+function showCalendar(month, year) {
+
+    var firstDay = ( new Date( year, month ) ).getDay();
+
+    tbl = document.getElementById("calendar-body");
+
+
+    tbl.innerHTML = "";
+
+
+    monthAndYear.innerHTML = months[month] + " " + year;
+    selectYear.value = year;
+    selectMonth.value = month;
+
+    // creating all cells
+    var date = 1;
+    for ( var i = 0; i < 6; i++ ) {
+
+        var row = document.createElement("tr");
+
+
+        for ( var j = 0; j < 7; j++ ) {
+            if ( i === 0 && j < firstDay ) {
+                cell = document.createElement( "td" );
+                cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            } else if (date > daysInMonth(month, year)) {
+                break;
+            } else {
+                cell = document.createElement("td");
+                cell.setAttribute("data-date", date);
+                cell.setAttribute("data-month", month + 1);
+                cell.setAttribute("data-year", year);
+                cell.setAttribute("data-month_name", months[month]);
+                cell.className = "date-picker";
+                cell.innerHTML = "<span>" + date + "</span>";
+
+                if ( date === today.getDate() && year === today.getFullYear() && month === today.getMonth() ) {
+                    cell.className = "date-picker selected";
+                }
+                row.appendChild(cell);
+                date++;
+            }
+
+
         }
+
+        tbl.appendChild(row);
     }
 
-    for (let i = 1; i <= monthDays; i++){
-        monthDaysText += '<li>' + i + '</li>';
-    }
-
-    daysContainer.innerHTML = monthDaysText;
-
-    if (month == nowMonth && year == nowYear){
-        days = daysContainer.getElementsByTagName('li');
-        days[monthPrefix + nowDateNumber - 1].classList.add('date-now');
-    }
 }
 
-setMonthCalendar(nowYear,nowMonth);
-
-prev.onclick = function () {
-    let curDate = new Date(yearContainer.textContent,monthName.indexOf(monthContainer.textContent));
-
-    curDate.setMonth(curDate.getMonth() - 1);
-
-    let curYear = curDate.getFullYear(),
-        curMonth = curDate.getMonth();
-
-    setMonthCalendar(curYear,curMonth);
-}
-
-next.onclick = function () {
-    let curDate = new Date(yearContainer.textContent,monthName.indexOf(monthContainer.textContent));
-
-    curDate.setMonth(curDate.getMonth() + 1);
-
-    let curYear = curDate.getFullYear(),
-        curMonth = curDate.getMonth();
-
-    setMonthCalendar(curYear,curMonth);
+function daysInMonth(iMonth, iYear) {
+    return 32 - new Date(iYear, iMonth, 32).getDate();
 }
