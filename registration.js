5ee81bc7ac9c03723000f0647bc2de0347fb577c
Williamc Commando <williamc@student.21-school.ru>
2022-10-15 19:04:24 +0300

    Теперь из формы регистрации достаются введённые данные

---
+++ registration.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('form');
+function getFormValue(event) {
+    event.preventDefault();
+    const fields = document.querySelectorAll('input, select, text, textarea');
+    const values = {};
+    fields.forEach(field => {
+        const {name, value} = field;
+        values[name] = value;
+    });
+    console.log(values);
+}
+form.addEventListener('submit', getFormValue);
